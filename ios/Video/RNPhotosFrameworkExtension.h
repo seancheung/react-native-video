10455be767ecc1dcfce085bc0ec113e9043ffe59
xuan.zhang <xuan.zhang@xiaobao100.com>
2021-05-28 12:15:57 +0800

    feat: add support for ios ph assets

---
+++ ios/Video/RNPhotosFrameworkExtension.h
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+#import <React/RCTView.h>
+#import <React/RCTConvert.h>
+
+@import Photos;
+@interface RNPhotosFrameworkExtension : NSObject
+typedef void (^RNPFVIdeoLoadCompleteBlock) (NSDictionary *source, AVAsset *asset, PHImageRequestID imageRequestID);
+
+-(NSString *)startLoadingPhotosAsset:(NSDictionary *)source bufferingCallback:(RCTBubblingEventBlock) onVideoBuffer andReactTag:(NSString *)reactTag andCompleteBlock:(RNPFVIdeoLoadCompleteBlock)completeBlock;
+
+@property (nonatomic, strong) NSString *loadedPhotosLocalIdentifier;
+@property (nonatomic) PHImageRequestID imageRequestID;
+
+@end
